31f59d37654150537e6f2bc0c3d0da7827c15d3e
Jing Chen <jic002@ucsd.edu>
2013-08-03 05:34:30 +0000

    changed url of the edge-relation function.

--- src/edu/sdsc/ontoquest/rest/BaseBean.java
+++ src/edu/sdsc/ontoquest/rest/BaseBean.java
@@ -11,14 +11,14 @@ import edu.sdsc.ontoquest.db.DbBasicFunctions;
 import edu.sdsc.ontoquest.query.Variable;
 
 /**
- * @version $Id: BaseBean.java,v 1.5 2013-08-02 22:14:00 jic002 Exp $
+ * @version $Id: BaseBean.java,v 1.6 2013-08-03 05:34:30 jic002 Exp $
  * Base bean class that supports common behaviors or attributes shared by
  * all beans.
  * 
  */
 public abstract class BaseBean {
-	public enum InputType { OID, NAME, TERM }
-	public enum NeighborType { PARENTS, CHILDREN, SUBCLASSES, SUPERCLASSES, PARTS, WHOLE, ALL,EDGE }
+	public enum InputType { OID, NAME, TERM, ID }
+	public enum NeighborType { PARENTS, CHILDREN, SUBCLASSES, SUPERCLASSES, PARTS, WHOLE, ALL,EDGE_RELATION }
 
 	public enum SiblingsType { CLASSES, PARTS }
 
