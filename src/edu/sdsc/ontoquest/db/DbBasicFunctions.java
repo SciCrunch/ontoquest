aefd941d44cfaea814783276376770332a24d086
Jing Chen <jic002@ucsd.edu>
2013-10-14 06:32:27 +0000

    BUGZID:

--- src/edu/sdsc/ontoquest/db/DbBasicFunctions.java
+++ src/edu/sdsc/ontoquest/db/DbBasicFunctions.java
@@ -28,7 +28,7 @@ import java.sql.SQLException;
 //import org.jgrapht.graph.DefaultDirectedGraph;
 
 /**
- * @version $Id: DbBasicFunctions.java,v 1.4 2013-09-25 17:34:09 jic002 Exp $
+ * @version $Id: DbBasicFunctions.java,v 1.5 2013-10-14 06:32:27 jic002 Exp $
  *
  */
 public class DbBasicFunctions implements BasicFunctions {
@@ -556,9 +556,16 @@ public class DbBasicFunctions implements BasicFunctions {
     return DbUtility.executeSQLQuery( sql, context, BaseBean.getVarList8(), null, errmsg);
   }
 
-  public ResourceSet searchpartOf ( int kbid, String term_id, Context context) throws OntoquestException
+  public ResourceSet searchHasPart ( int kbid, String term_id, Context context) throws OntoquestException
   {
     String sql = "select * from get_outgoing_enclosure_by_term_id('"+term_id+"','part_of',"+kbid+")";
+    String errmsg = "";
+    return DbUtility.executeSQLQuery( sql, context, BaseBean.getVarList8(), null, errmsg);
+  }
+
+  public ResourceSet searchpartOf ( int kbid, String term_id, Context context) throws OntoquestException
+  {
+    String sql = "select * from get_incoming_enclosure_by_term_id('"+term_id+"','part_of',"+kbid+")";
     String errmsg = "";
     return DbUtility.executeSQLQuery( sql, context, BaseBean.getVarList8(), null, errmsg);
   }
