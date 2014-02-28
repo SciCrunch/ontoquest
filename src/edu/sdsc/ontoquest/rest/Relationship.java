1cc13fedb46f89e1219f621b110254ed77140807
Jing Chen <jingjingbic@gmail.com>
2014-02-27 17:12:30 -0800

    Added more properties to the class to avoid getting them on the fly.

--- src/edu/sdsc/ontoquest/rest/Relationship.java
+++ src/edu/sdsc/ontoquest/rest/Relationship.java
@@ -11,16 +11,19 @@ import org.w3c.dom.Element;
  * @version $Id: Relationship.java,v 1.5 2013-10-22 19:36:17 jic002 Exp $
  *
  */
-public class Relationship extends BaseBean {
+public class Relationship extends BaseBean implements Comparable <Relationship> {
   private int rid1; // A (rid, rtid) pair uniquely identifies a node in ontology graph.
   private int rtid1;
+  private String name1;
   private String label1;
   private int rid2;
   private int rtid2;
   private String label2;
+  private String name2;
   private int pid; // property id, a.k.a. edge label id
-  private String pname; // property name, a.k.a. edge label
-  private Context context;
+  private String pname; // property name, a.k.a. edge label. It is property name not label in some cases.
+  private String pExtId;  // this is the name of the property. 
+//  private Context context;
   
   private static final int DEFAULT_PROPERTY_RTID = 15;
   
@@ -28,7 +31,8 @@ public class Relationship extends BaseBean {
   private static final int fODD_PRIME_NUMBER = 37;
 
   
-  public Relationship(int rid1, int rtid1, String label1, int rid2, int rtid2, String label2, int pid, String pname, Context context) {
+  public Relationship(int rid1, int rtid1, String label1, int rid2, int rtid2, String label2, int pid, String pname, Context context) throws OntoquestException
+  {
     this.rid1 = rid1;
     this.rtid1 = rtid1;
     this.label1 = label1;
@@ -37,8 +41,27 @@ public class Relationship extends BaseBean {
     this.label2 = label2;
     this.pid = pid;
     this.pname = pname;
-    this.context = context;
+    this.name1 = ClassNode.generateExtId(getRid1(), getRtid1(), context);
+    this.name2 = ClassNode.generateExtId(getRid2(), getRtid2(), context);
+    this.pExtId = ClassNode.generateExtId(pid, DEFAULT_PROPERTY_RTID, context);
   }
+
+  public Relationship(int rid1, int rtid1, String label1, int rid2, int rtid2, String label2, int pid, String pname, 
+                      String name1 , String name2, String pExtId) 
+  {
+    this.rid1 = rid1;
+    this.rtid1 = rtid1;
+    this.label1 = label1;
+    this.rid2 = rid2;
+    this.rtid2 = rtid2;
+    this.label2 = label2;
+    this.pid = pid;
+    this.pname = pname;
+    this.name1 = name1;
+    this.name2 = name2;
+    this.pExtId = pExtId;
+  }
+
 
   /**
    * @return the rid1
@@ -122,13 +145,8 @@ public class Relationship extends BaseBean {
     
     Element subElem = doc.createElement("subject");
     subElem.setAttribute(ClassNode.interanIdAttrName, ClassNode.generateId(getRid1(), getRtid1()));
-    try
-    {
-      subElem.setAttribute("id", ClassNode.generateExtId(getRid1(), getRtid1(), context));
-    } catch (OntoquestException f)
-    {
-       System.out.println("Exception from Relationship.toXML()" + f.getMessage());
-    }
+      subElem.setAttribute("id", name1);
+
     if ( getLabel1() != null ) {
       subElem.appendChild(doc.createTextNode(getLabel1()));
       e.appendChild(subElem);
@@ -137,13 +155,7 @@ public class Relationship extends BaseBean {
     
     Element propElem = doc.createElement("property");
     propElem.setAttribute(ClassNode.interanIdAttrName, ClassNode.generateId(getPid(), DEFAULT_PROPERTY_RTID));
-    try
-    {
-      propElem.setAttribute("id", ClassNode.generateExtId(getPid(), DEFAULT_PROPERTY_RTID,context));
-    } catch (OntoquestException f)
-    {
-      System.out.println("Exception from Relationship.toXML()" + f.getMessage());
-    }
+    propElem.setAttribute("id", pExtId);
     
     if ( getPname() != null ) {
       propElem.appendChild(doc.createTextNode(getPname()));
@@ -154,13 +166,7 @@ public class Relationship extends BaseBean {
     
     Element objElem = doc.createElement("object");
     objElem.setAttribute(ClassNode.interanIdAttrName, ClassNode.generateId(getRid2(), getRtid2()));
-    try
-    {
-      objElem.setAttribute("id", ClassNode.generateExtId(getRid2(), getRtid2(),context));
-    } catch (OntoquestException f)
-    {
-      System.out.println("Exception from Relationship.toXML()" + f.getMessage());
-    }
+    objElem.setAttribute("id", name2);
     if ( getLabel2() != null) {
       objElem.appendChild(doc.createTextNode(getLabel2()));
       e.appendChild(objElem);
@@ -168,5 +174,27 @@ public class Relationship extends BaseBean {
       System.out.println("ERROR: getLabel2 returns null for "+ rid2 + ',' + rtid2);
 
     return e;
+  }
+
+
+  @Override
+  public int compareTo(Relationship o)
+  {
+    if ( o.getRid1() > rid1 ) return 1;
+    if ( o.getRid1() < rid1 ) return -1;
+
+    if ( o.getRid2() > rid2 ) return 1;
+    if ( o.getRid2() < rid2 ) return -1;
+    
+    if ( o.getRtid1() > rtid1) return 1 ;
+    if ( o.getRtid1() < rtid1) return -1 ;
+    
+    if ( o.getRtid2() > rtid2) return 1 ;
+    if ( o.getRtid2() < rtid2) return -1;
+
+    if ( o.getPid() > pid) return 1 ;
+    if ( o.getPid() < pid) return -1 ;
+    
+    return 0;
   }
 }
